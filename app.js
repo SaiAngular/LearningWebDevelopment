51e09776b5942fe5435325c8fee673a97104f65a
saikiran <pillellasai@gmail.com>
2022-05-20 21:06:36 +0530

    functions execution

---
+++ app.js
@@ -0,0 +1,23 @@
+function a() {
+    debugger
+    console.log("Hello Andhra");
+};
+
+
+function b() {
+
+    debugger
+    console.log("Hello world");
+
+}
+
+function c() {
+    debugger
+
+    console.log("Hello India");
+
+}
+
+c();
+b();
+a();
